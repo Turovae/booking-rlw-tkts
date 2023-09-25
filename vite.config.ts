91375bcae57cd0989b4de340e0f9afdf4ad22c6f
Turovae <ekaterina.turova@mail.ru>
2023-09-25 23:12:08 +0700

    Добавлены настройки развертывания на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(( {command} ) => {
+  const setting = {
+    plugins: [react()],
+    base: '',
+  };
+
+  if (command === 'build') setting.base = 'https://turovae.github.io/booking-rlw-tkts/';
+
+  return setting;
+});
