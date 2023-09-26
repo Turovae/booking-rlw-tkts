8dddfcf45b827b98008c70ffcaab49e40a00fa75
Turovae <ekaterina.turova@mail.ru>
2023-09-26 23:40:04 +0700

    Добавлены пункты меню в шапку главной страницы

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ function App() {
     <div className='main'>
       <div className="header">
         <div className="header__banner"></div>
-        <div className="header__logo">Logo</div>
+        <div className="header__logo">Лого</div>
+        <ul className="header__menu menu">
+          <li className="menu__item"><a href="#">О нас</a></li>
+          <li className="menu__item"><a href="#">Как это работает</a></li>
+          <li className="menu__item"><a href="#">Отзывы</a></li>
+          <li className="menu__item"><a href="#">Контакты</a></li>
+        </ul>
       </div>
       <div className="about">About us</div>
       <div className="how-works">How it works</div>
