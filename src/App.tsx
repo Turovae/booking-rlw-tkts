d69d608c2b9017d0427b4083179e0a8623a3fe0b
Turovae <ekaterina.turova@mail.ru>
2023-09-26 22:45:38 +0700

    Добавлен лого в блок header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='main'>
       <div className="header">
+        <div className="header__banner"></div>
         <div className="header__logo">Logo</div>
       </div>
       <div className="about">About us</div>
