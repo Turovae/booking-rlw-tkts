cd3ff391448883ebbf2d2be12b4c3fb8cae2b478
Turovae <ekaterina.turova@mail.ru>
2023-09-26 14:35:12 +0700

    Добавлен шрифт Roboto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+// import './App.css'
 
 function App() {
   return (
     <div className='main'>
-      <div className="header">Header</div>
+      <div className="header">
+        <div className="header__logo">Logo</div>
+      </div>
       <div className="about">About us</div>
       <div className="how-works">How it works</div>
       <div className="feedback">Feedback</div>
