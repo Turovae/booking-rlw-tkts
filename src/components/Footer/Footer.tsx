99361ab8fe38d1ec8062d016474b6235692eed23
Turovae <ekaterina.turova@mail.ru>
2023-10-11 23:19:49 +0700

    Создан компонент Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,23 @@
+import Logo from '../Logo/Logo';
 import './Footer.scss'
 
 function Footer () {
   return (
     <div className="footer">
-      This is contacts block
+      <div className="footer__top">
+        <div className="footer__inline container">
+          This is contacts block
+          <div className="footer__col"></div>
+          <div className="footer__col"></div>
+        </div>
+      </div>
+      <div className="footer__bottom">
+        <div className="footer__inline container">
+          <Logo />
+          <a href=''>top</a>
+          <div>2018 WEB</div>
+        </div>
+      </div>
     </div>
   )
 }
