aef5c37a02e5bcc43e32cf5a00b7f9e489bdd059
Turovae <ekaterina.turova@mail.ru>
2023-10-17 22:13:08 +0700

    добавлена библиотека moment

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import './App.scss';
+import Calendar from './components/Calendar/Calendar';
 
-import About from "./components/About/About";
-import Header from "./components/Header/Header";
-import HowWorks from "./components/HowWorks/HowWorks";
-import LoadLine from "./components/LoadLine/LoadLine";
-import Feedback from "./components/Feedback/Feedback";
-import Footer from "./components/Footer/Footer";
+// import About from "./components/About/About";
+// import Header from "./components/Header/Header";
+// import HowWorks from "./components/HowWorks/HowWorks";
+// import LoadLine from "./components/LoadLine/LoadLine";
+// import Feedback from "./components/Feedback/Feedback";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className='page'>
-      <Header />
+      {/* <Header />
       <LoadLine progress={55} />
       <About />
       <HowWorks />
       <Feedback />
-      <Footer />
+      <Footer /> */}
+      <Calendar />
     </div>
   )
 }
---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,7 @@
+function Calendar() {
+  return (
+    <div className="calendar">Calendar</div>
+  )
+}
+
+export default Calendar;
