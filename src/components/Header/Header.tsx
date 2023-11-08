082008f32d4bff434ffe4df67b9a33b1e3df96bd
Turovae <ekaterina.turova@mail.ru>
2023-11-08 13:21:54 +0700

    search-form вынесена в отдельный компонент

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Logo from '../Logo/Logo';
+import SearchForm from '../SearchForm/SearchForm';
 import './Header.scss';
 
 function Header() {
@@ -26,30 +27,8 @@ function Header() {
             <span className="header__slogan_bold">путешествие!</span>
           </h2>
         </div>
-        <div className="header__col">
-          <form className="header__form search-form" action="">
-            <div className="search-form__row">
-              <span className="search-form__hint">Направление</span>
-              <div className="search-form__inputs">
-                <input type="search" list="cities" className="search-form__input" name="lacation-from" placeholder="Откуда" />
-                <button type="button" className="search-form__direction-change" />
-                <input type="text" list="cities" className="search-form__input" name="lacation-from" placeholder="Куда" />
-                <datalist id="cities">
-                  {cityes.map((city) => <option key={city} value={city}>{city}</option>)}
-                </datalist>
-              </div>
-            </div>
-            <div className="search-form__row">
-              <span className="search-form__hint">Дата</span>
-              <div className="search-form__inputs">
-                <input type="date" className="search-form__input" name="lacation-from" />
-                <input type="date" className="search-form__input" name="lacation-from" />
-              </div>
-            </div>
-            <div className="search-form__row">
-              <button className="search-form__button-submit" type='button'>Найти билеты</button>
-            </div>
-          </form>
+        <div className="header__col header__form">
+          <SearchForm />
         </div>
       </div>
     </div>
---
+++ src/components/SearchForm/SearchForm.tsx
@@ -0,0 +1,33 @@
+import './SearchForm.scss';
+
+function SearchForm() {
+  const cityes = ['moskow', 'novosibirsk', 'krasnoyarsk', 'kemerovo', 'achinsk'];
+
+  return (
+    <form className="search-form" action="">
+      <div className="search-form__row">
+        <span className="search-form__hint">Направление</span>
+        <div className="search-form__inputs">
+          <input type="search" list="cities" className="search-form__input" name="lacation-from" placeholder="Откуда" />
+          <button type="button" className="search-form__direction-change" />
+          <input type="text" list="cities" className="search-form__input" name="lacation-from" placeholder="Куда" />
+          <datalist id="cities">
+            {cityes.map((city) => <option key={city} value={city}>{city}</option>)}
+          </datalist>
+        </div>
+      </div>
+      <div className="search-form__row">
+        <span className="search-form__hint">Дата</span>
+        <div className="search-form__inputs">
+          <input type="date" className="search-form__input" name="lacation-from" />
+          <input type="date" className="search-form__input" name="lacation-from" />
+        </div>
+      </div>
+      <div className="search-form__row">
+        <button className="search-form__button-submit" type='button'>Найти билеты</button>
+      </div>
+    </form>
+  )
+}
+
+export default SearchForm;
